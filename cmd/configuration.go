e89318fbb663af281a5545c56c5a124015bf3cd4
9bany <bany.y0599@gmail.com>
2022-06-25 16:10:11 +0700

    ft/configuration-for-the-main-file

---
+++ cmd/configuration.go
@@ -0,0 +1,72 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Watch contains all extensions to be observed
+type Watch struct {
+	Files      []string `yaml:"file"`
+	Extensions []string `yaml:"extensions"`
+}
+
+// Ignore contains all file and extension
+// to be not observed during the application cycle
+type Ignore struct {
+	Files      []string `yaml:"file"`
+	Extensions []string `yaml:"extensions"`
+}
+
+// Configuration is the main file config for the ron app
+// All the informations and config in the ron.yml
+// And we convert it to the Configuration struct
+type Configuration struct {
+	RootPath string `yaml:"root_path"`
+	ExecPath string `yaml:"exec_path"`
+	Language string `yaml:"language"`
+}
+
+// Init is the configuration function 
+// with responsibility to read and defining the configuration
+func InitConfing() (*Configuration, error) {
+	yml, err := checkFileIfExist()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return getConf(yml)
+}
+
+// getConf: read the main file for ron
+// and decode it into a new Configuration instance
+func getConf(fileName string) (*Configuration, error) {
+	var config *Configuration
+	yamlFile, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal(yamlFile, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+// Check the main file if exist return nil
+// or not, will return error
+func checkFileIfExist() (string, error) {
+	yml := fmt.Sprintf("%s.%s", FILE_NAME, EXTENSION)
+
+	if _, err := os.Stat(yml); !os.IsNotExist(err) {
+		return yml, nil
+	}
+
+	return "", errors.New("ron.yml file not found")
+}
---
+++ cmd/constans.go
@@ -0,0 +1,8 @@
+package cmd
+
+const (
+	// The file name for the main system file
+	FILE_NAME = "ron"
+	// The extension of the file for the main system file
+	EXTENSION = "yml"
+)
