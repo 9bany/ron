f0eb18394f575026177757c77e22729b18e0016b
9bany <bany.y0599@gmail.com>
2022-06-26 18:40:02 +0700

    restart server then it done

--- examples/nodejs/index.js
+++ examples/nodejs/index.js
@@ -1,2 +1,9 @@
-console.log("ok");
-alsdf
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.write('Hello, im hdjjk bany');
+  res.end();
+
+}).listen(3000, function(){
+ console.log("server start at port 3000");
+});  
