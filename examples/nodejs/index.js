cf70ce8082f153631da01a4715ae4bd8c2cf46a9
9bany <bany.y0599@gmail.com>
2022-06-26 23:02:16 +0700

    add ignore files

--- examples/nodejs/index.js
+++ examples/nodejs/index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Hello, im hdjjk bany');
+  res.write('Hello, im  bany');
   res.end();
 
 }).listen(3000, function(){
