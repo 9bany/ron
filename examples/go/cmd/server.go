8efa1c00a8923512f6c2d99c14818492ec56b519
9bany <bany.y0599@gmail.com>
2022-06-28 16:56:55 +0700

    fix/go-example

--- examples/go/cmd/server.go
+++ examples/go/cmd/server.go
@@ -12,6 +12,10 @@ func main() {
 		w.Write([]byte("some"))
 	})
 
+	http.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok api"))
+	})
+
 	fmt.Println("server start at port 3000")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal(err.Error())
