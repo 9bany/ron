54d9465ff1d57a461643623d3359fa8d64121204
9bany <bany.y0599@gmail.com>
2022-06-28 09:09:45 +0700

    ft/auto-relisten-when-create-new:

--- examples/nodejs/index.js
+++ examples/nodejs/index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Hello, some time it will be ok change by bany');
+  res.write("server");
   res.end();
 
 }).listen(3000, function(){
