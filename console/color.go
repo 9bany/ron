1d6b5527aeda3972305581b8b51096afd2368113
@bany <53461381+9bany@users.noreply.github.com>
2022-06-28 08:50:00 +0700

    Ft/update colors (#11)
    * ft/update-colors
    
    * change emoji

--- console/color.go
+++ console/color.go
@@ -1,7 +1,8 @@
 package console
 
 var (
-	bCyan = "\033[1;36m"
-	bRed  = "\033[1;31m"
-	reset = "\033[0m"
+	bRed    = "\033[1;31m"
+	reset   = "\033[0m"
+	bPurple = "\033[1;35m"
+	bGreen  = "\033[1;32m"
 )
